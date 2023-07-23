023853c570d8f1c42e596ec24369d34b67cf6359
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-07-23 21:05:07 +0800

    Test

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = useState('');
+
+  const onPressLearnMore = () => {
+    Alert.alert('Alert Title', text, [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+      <Text>Open up App.js to start working on your app epal ka bonak! editowsss</Text>
       <StatusBar style="auto" />
+
+      <Button
+        onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
